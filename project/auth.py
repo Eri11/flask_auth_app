dd89c86d512e397c7b93bf865403d2b05433f75a
Eri11 <erikaeli11@gmail.com>
2022-03-21 07:57:50 -0700

    base de datos con todos los campos y hash

--- project/auth.py
+++ project/auth.py
@@ -13,6 +13,7 @@ def login():
 @auth.route('/login', methods=['POST'])
 def login_post():
     # login code goes here
+       
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
@@ -22,7 +23,7 @@ def login_post():
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
+        flash('Usuario y Contraseña no coinciden, intenta de nuevo.')
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
@@ -36,18 +37,22 @@ def signup():
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     # code to validate and add user to database goes here
-    email = request.form.get('email')
     name = request.form.get('name')
+    lastname = request.form.get('lastname')
+    address = request.form.get('address')
+    tel = request.form.get('tel')
+
+    email = request.form.get('email')
     password = request.form.get('password')
 
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
+        flash('El correo electrónico ya existe')
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(name=name, lastname=lastname, address=address, tel=tel, email=email,  password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
     db.session.add(new_user)
--- project/models.py
+++ project/models.py
@@ -6,3 +6,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    lastname = db.Column(db.String(1000))
+    address = db.Column(db.String(1000))
+    tel = db.Column(db.String(100))

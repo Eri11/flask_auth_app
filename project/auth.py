bd94d9c4e0d8959c84b46445d8f212374a3c7de5
Eri11 <erikaeli11@gmail.com>
2022-03-26 17:23:17 -0700

    setup base - asimmetrical

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-#from werkzeug.security import generate_password_hash, check_password_hash
+
+#this is for ASIMMETRICAL
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+import base64 
+
 from flask_login import login_user, login_required, logout_user
 from password_strength import PasswordPolicy
 from password_strength import PasswordStats
@@ -17,29 +23,23 @@ policy = PasswordPolicy.from_names(
     strength=0.66, #need a password that scires at least 0.66 with its entropy bits
 )
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
+key = 'mysecretpassword'.encode('utf-8')
+iv = 'myivsupersecreta'.encode('utf-8')
 
-@auth.route('/login', methods=['POST'])
-def login_post():
-    # login code goes here
-       
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+def encrypt(password, key, iv):
+    stringToBytes = pad(password.encode('utf-8'), AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = base64.b64encode(cipher.encrypt(stringToBytes))
 
-    user = User.query.filter_by(email=email).first()
+    return ciphertext
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
-        flash('Usuario y Contraseña no coinciden, intenta de nuevo.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+def decrypt(encryptedPass, key, iv):
+    decodedPass = base64.b64decode(encryptedPass)
+    decipher = AES.new(key, AES.MODE_CBC, iv)
+    deciphertext = unpad(decipher.decrypt(decodedPass), AES.block_size).decode('utf-8')
 
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return deciphertext
+
 
 @auth.route('/signup')
 def signup():
@@ -71,16 +71,50 @@ def signup_post():
         flash("La contraseña no es lo suficientemente compleja. Evite usar caraceres consecutivos y palabras fáciles de adivinar.")
         return redirect(url_for('auth.signup'))
     else:
-        print(stats.strength())
-        
+        print(stats.strength()) #remover en produccion
+
+        encryptedPass = encrypt(password, key, iv)
+        print(f'Encrypted PWD: {encryptedPass}')
+
+                
+               
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-        new_user = User(name=name, lastname=lastname, address=address, tel=tel, email=email, password=generate_password_hash(password, method='sha256'))
+        new_user = User(name=name, lastname=lastname, address=address, tel=tel, email=email, password=encryptedPass)
 
         # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
 
         return redirect(url_for('auth.login'))
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+       
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+    # check if the user actually exists
+
+    encryptedPass = user.password
+
+    dbPass = decrypt(encryptedPass, key, iv)
+   
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or (dbPass != password):
+        flash('Usuario y Contraseña no coinciden, intenta de nuevo.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
 
 @auth.route('/logout')
 @login_required
